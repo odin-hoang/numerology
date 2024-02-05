b90fc1faaaf386ff01231d15ccc82a883458277e
uKaxg <uka.pgr@gmail.com>
2024-02-05 20:45:19 +0700

    add conditional rulling number render

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,6 @@ export default function Home() {
   const [rullingNumber, setRullingNumber] = useState(0);
   const [birthChart, setBirthChart] = useState([0, 3, 2, 0, 0, 0, 0, 0, 0]);
   const [nameChart, setNameChart] = useState([1, 1, 0, 0, 0, 0, 0, 1, 1]);
-  const name = "khai";
   return (
     <main className="flex flex-col items-center justify-between px-12 gap-5 pb-12">
       <Header />
@@ -111,64 +110,87 @@ export default function Home() {
           <Button type="submit">Tra cứu</Button>
         </form>
       </Form>
-      <Separator></Separator>
-      <h2>
-        Số chủ đạo{" "}
-        <span className="bg-gradient-to-t inline-block p-2 from-pink-500 to bg-yellow-400 rounded-full w-10 h-10 text-center font-bold text-white background-animate">
-          {rullingNumber}
-        </span>
-      </h2>
-      <div>
-        <Table className="w-[210px] h-[210px]">
-          <TableCaption>
-            Biểu đồ ngày sinh + <span className="text-red-500">tên</span>
-          </TableCaption>
-          <TableBody>
-            <TableRow>
-              <TableCell className="border-r ">
-                <span>{"3".repeat(birthChart[2])}</span>
-                <span className="text-red-500">{"3".repeat(nameChart[2])}</span>
-              </TableCell>
-              <TableCell className="border-r">
-                <span>{"6".repeat(birthChart[5])}</span>
-                <span className="text-red-500">{"6".repeat(nameChart[5])}</span>
-              </TableCell>
-              <TableCell>
-                <span>{"9".repeat(birthChart[8])}</span>
-                <span className="text-red-500">{"9".repeat(nameChart[8])}</span>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="border-r">
-                <span>{"2".repeat(birthChart[1])}</span>
-                <span className="text-red-500">{"2".repeat(nameChart[1])}</span>
-              </TableCell>
-              <TableCell className="border-r">
-                <span>{"5".repeat(birthChart[4])}</span>
-                <span className="text-red-500">{"5".repeat(nameChart[4])}</span>
-              </TableCell>
-              <TableCell>
-                <span>{"8".repeat(birthChart[7])}</span>
-                <span className="text-red-500">{"8".repeat(nameChart[7])}</span>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="border-r">
-                <span>{"1".repeat(birthChart[0])}</span>
-                <span className="text-red-500">{"1".repeat(nameChart[0])}</span>
-              </TableCell>
-              <TableCell className="border-r">
-                <span>{"4".repeat(birthChart[3])}</span>
-                <span className="text-red-500">{"4".repeat(nameChart[3])}</span>
-              </TableCell>
-              <TableCell>
-                <span>{"7".repeat(birthChart[6])}</span>
-                <span className="text-red-500">{"7".repeat(nameChart[6])}</span>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
+
+      {!!rullingNumber && (
+        <>
+          <Separator></Separator>
+          <h2>
+            Số chủ đạo{" "}
+            <span className="bg-gradient-to-t inline-block p-2 from-pink-500 to bg-yellow-400 rounded-full w-10 h-10 text-center font-bold text-white background-animate">
+              {rullingNumber}
+            </span>
+          </h2>
+          <div>
+            <Table className="w-[210px] h-[210px]">
+              <TableCaption>
+                Biểu đồ ngày sinh + <span className="text-red-500">tên</span>
+              </TableCaption>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="border-r ">
+                    <span>{"3".repeat(birthChart[2])}</span>
+                    <span className="text-red-500">
+                      {"3".repeat(nameChart[2])}
+                    </span>
+                  </TableCell>
+                  <TableCell className="border-r">
+                    <span>{"6".repeat(birthChart[5])}</span>
+                    <span className="text-red-500">
+                      {"6".repeat(nameChart[5])}
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span>{"9".repeat(birthChart[8])}</span>
+                    <span className="text-red-500">
+                      {"9".repeat(nameChart[8])}
+                    </span>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="border-r">
+                    <span>{"2".repeat(birthChart[1])}</span>
+                    <span className="text-red-500">
+                      {"2".repeat(nameChart[1])}
+                    </span>
+                  </TableCell>
+                  <TableCell className="border-r">
+                    <span>{"5".repeat(birthChart[4])}</span>
+                    <span className="text-red-500">
+                      {"5".repeat(nameChart[4])}
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span>{"8".repeat(birthChart[7])}</span>
+                    <span className="text-red-500">
+                      {"8".repeat(nameChart[7])}
+                    </span>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="border-r">
+                    <span>{"1".repeat(birthChart[0])}</span>
+                    <span className="text-red-500">
+                      {"1".repeat(nameChart[0])}
+                    </span>
+                  </TableCell>
+                  <TableCell className="border-r">
+                    <span>{"4".repeat(birthChart[3])}</span>
+                    <span className="text-red-500">
+                      {"4".repeat(nameChart[3])}
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span>{"7".repeat(birthChart[6])}</span>
+                    <span className="text-red-500">
+                      {"7".repeat(nameChart[6])}
+                    </span>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+        </>
+      )}
     </main>
   );
 }
